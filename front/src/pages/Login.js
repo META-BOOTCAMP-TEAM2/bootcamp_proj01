29897f00524281910ba21b2e07887198b6428ef0
kimYHgit <younghoun95@naver.com>
2023-06-16 09:17:59 +0900

    페이지 일부수정

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,102 +1,66 @@
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState, useContext } from "react";
-import { AuthContext } from "../assets/AuthContext";
+// import axios from "axios";
+// import React, { useState, useContext } from "react";
+// import { AuthContext } from "../assets/AuthContext";
+import Google from "../assets/google.png";
+import Kakao from "../assets/KakaoTalk_logo.svg";
 
 const LoginForm = () => {
-  const [userEmail, setUserEmail] = useState("");
-  const [password, setPassword] = useState("");
-  // const [passwordError, setpasswordError] = useState("");
-  // const [emailError, setmailError] = useState("");
-  const { setAuthState } = useContext(AuthContext);
-
-  const login = () => {
-    const data = { userEmail: userEmail, password: password };
-    axios.post("http://localhost:8000/auth/login", data).then((response) => {
-      if (response.data.error) {
-        alert(response.data.error);
-      } else {
-        localStorage.setItem("accessToken", response.data.token);
-        setAuthState({
-          useremail: response.data.useremail,
-          id: response.data.id,
-          status: true,
-        });
-      }
-    });
+  const google = () => {
+    window.open("http://localhost:8000/auth/google", "_self");
   };
 
+  const Kakao = () => {
+    window.open("http://localhost:8000/auth/kakao", "_self");
+  };
+
+  // const [userEmail, setUserEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // // const [passwordError, setpasswordError] = useState("");
+  // // const [emailError, setmailError] = useState("");
+  // const { setAuthState } = useContext(AuthContext);
+
+  // const login = () => {
+  //   const data = { userEmail: userEmail, password: password };
+  //   axios.post("http://localhost:8000/auth/login", data).then((response) => {
+  //     if (response.data.error) {
+  //       alert(response.data.error);
+  //     } else {
+  //       localStorage.setItem("accessToken", response.data.token);
+  //       setAuthState({
+  //         useremail: response.data.useremail,
+  //         id: response.data.id,
+  //         status: true,
+  //       });
+  //     }
+  //   });
+  // };
+
   return (
-    // <div>
     //   <h1>로그인</h1>
     //   <p>서비스 이용을 위해 로그인해주세요</p>
-    //   <div className="loginContainer">
-    //     <label>UserEmail:</label>
-    //     <input
-    //       type="text"
-    //       onChange={(event) => {
-    //         setUserEmail(event.target.value);
-    //       }}
-    //     />
-    //     <label>Password:</label>
-    //     <input
-    //       type="password"
-    //       onChange={(event) => {
-    //         setPassword(event.target.value);
-    //       }}
-    //     />
 
-    //     <button onClick={login}> Login </button>
-    //   </div>
-    // </div>
-    <div className="container">
-      <div className="row d-flex justify-content-center">
-        <div className="col-md-4">
-          <form id="loginform" onSubmit={login}>
-            <div className="form-group">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="EmailInput"
-                name="EmailInput"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-                onChange={(event) => setUserEmail(event.target.value)}
-              />
-              {/* <small id="emailHelp" className="text-danger form-text">
-                {emailError}
-              </small> */}
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-              {/* <small id="passworderror" className="text-danger form-text">
-                {passwordError}
-              </small> */}
-            </div>
-            <div className="form-group form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id="exampleCheck1"
-              />
-              <label className="form-check-label">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+    <div className="login">
+      <h1 className="loginTitle">서비스 이용을 위해 로그인해주세요</h1>
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton google" onClick={google}>
+            <img src={Google} alt="" className="icon" />
+            Google
+          </div>
+          <div className="loginButton Kakao" onClick={Kakao}>
+            <img src={Kakao} alt="" className="icon" />
+            Kakao
+          </div>
         </div>
-        {/* 
-        Source: <a href="https://askavy.com/react-form/">React Form</a>
-       */}
+        <div className="center">
+          <div className="line" />
+          <div className="or">OR</div>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button className="submit">Login</button>
+        </div>
       </div>
     </div>
   );
