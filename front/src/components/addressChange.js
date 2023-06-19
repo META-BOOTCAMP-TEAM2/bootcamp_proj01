e5684849b2ce737874f75ea1ec833d587a38d5a9
DongbinL <gdblee0410@gmail.com>
2023-06-19 19:48:44 +0900

    페이지 이동 수정

---
+++ front/src/components/addressChange.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const change = async (address) => {
+  const apiKey = "AIzaSyAF--ZmANNDuao7xDJN08MoaDem3RJbtKg"; // Google Geocoding API 키
+
+  try {
+    const response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        address: address,
+        key: apiKey,
+      },
+    });
+
+    const results = response.data.results;
+    if (results.length > 0) {
+      const location = results[0].geometry.location;
+      const lat = location.lat;
+      const lng = location.lng;
+      return { lat, lng };
+    } else {
+      throw new Error("No results found.");
+    }
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
+
+const addressChange = async (address) => {
+  try {
+    const position = await change(address);
+    const formattedPosition = `${position.lat},${position.lng}`;
+    console.log(formattedPosition);
+    return position;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default addressChange;
